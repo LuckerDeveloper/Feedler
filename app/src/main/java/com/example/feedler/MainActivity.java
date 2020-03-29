99a92920312b18e56696e6d6754a28cac5407a9a
Rail <rail.ilyasov.98@mail.ru>
2020-03-30 01:43:00 +0300

    Auth

--- app/src/main/java/com/example/feedler/MainActivity.java
+++ app/src/main/java/com/example/feedler/MainActivity.java
@@ -1,16 +1,51 @@
 package com.example.feedler;
 
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
 
-public class MainActivity extends AppCompatActivity {
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.vk.sdk.VKScope;
+import com.vk.sdk.VKSdk;
+
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    private String[] scope = new String[]{VKScope.FRIENDS, VKScope.WALL};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // В этой Activity будет список постов с помощью RecyclerView
+        Button authButtonVK = findViewById(R.id.authButtonVK);
+        Button authButtonFB = findViewById(R.id.authButtonFB);
+        Button listButton = findViewById(R.id.listButton);
+
+        authButtonVK.setOnClickListener(this);
+        authButtonFB.setOnClickListener(this);
+        listButton.setOnClickListener(this);
+    }
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.authButtonVK:
+                if (VKSdk.isLoggedIn()) {
+                    Toast.makeText(getApplicationContext(),"Регистрация уже произведена",Toast.LENGTH_LONG).show();
+                } else {
+                    VKSdk.login(this, scope);
+                }
+                break;
+            case R.id.authButtonFB:
+                //Регистрация в Facebook
+                break;
+            case R.id.listButton:
+                Intent intent = new Intent(getApplicationContext(), ListActivity.class);
+                startActivity(intent);
+        }
 
     }
 }
--- app/src/main/java/com/example/feedler/PostRepository.java
+++ app/src/main/java/com/example/feedler/PostRepository.java
@@ -1,7 +1,7 @@
 package com.example.feedler;
 
 import android.app.Application;
-import android.arch.lifecycle.LiveData;
+import androidx.lifecycle.LiveData;
 
 import java.util.List;
 
