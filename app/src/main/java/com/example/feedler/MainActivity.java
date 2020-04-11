f37448fcb3dd87d5c078b5dcd70d61f38387ff2f
TokarevIP <tokarevrab@gmail.com>
2020-04-11 18:58:27 +0300

    v1.1

--- app/src/main/java/com/example/feedler/MainActivity.java
+++ app/src/main/java/com/example/feedler/MainActivity.java
@@ -7,8 +7,6 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ArrayAdapter;
-import android.widget.ListView;
 import android.widget.TextView;
 
 
