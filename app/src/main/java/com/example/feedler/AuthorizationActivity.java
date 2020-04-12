b7d9eaf587bc0fec78d9f2a96488ef907dbca8dc
TokarevIP <tokarevrab@gmail.com>
2020-04-12 14:03:55 +0300

    v1.1

--- app/src/main/java/com/example/feedler/AuthorizationActivity.java
+++ app/src/main/java/com/example/feedler/AuthorizationActivity.java
@@ -2,8 +2,7 @@ package com.example.feedler;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.app.Activity;
-import android.content.Intent;
+
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
