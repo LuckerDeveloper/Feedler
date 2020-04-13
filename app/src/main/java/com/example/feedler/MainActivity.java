1bc4f005cfe4bb8bfafe03f48733c57bf7148e88
Sasha <alfer.sasha@yandex.ru>
2020-04-13 13:52:08 +0300

    Добавил получение постов с интернета. Изменен тип даты в классе Post с Date на String!!!

--- app/src/main/java/com/example/feedler/MainActivity.java
+++ app/src/main/java/com/example/feedler/MainActivity.java
@@ -47,37 +47,5 @@ public class MainActivity extends AppCompatActivity {
                 arrayList.addAll(posts);
             }
         });
-
-
-
-
-
-
-//       Это будет в другом файле
-//
-//        VKRequest request = new VKRequest("newsfeed.get", VKParameters.from(VKApiConst.FILTERS, "post")); //Запрос с фильтром post
-//        request.executeWithListener(new VKRequest.VKRequestListener() {
-//                @Override
-//            public void onComplete(VKResponse response) {
-//                super.onComplete(response);
-//
-//                try {
-//                    JSONObject jsonObject = (JSONObject) response.json.get("response"); //получаем JSON объект по запросу
-//                    JSONArray jsonArray = (JSONArray) jsonObject.get("items"); //Получаем конкретно посты
-//                    for (int i = 0; i < jsonArray.length(); i++) {
-//                        JSONObject post = (JSONObject) jsonArray.get(i);
-//                        arrayList.add(post.getString("text")); //Производим перебор и получаем то, что является телом поста
-//                    }
-//                    arrayAdapter = new ArrayAdapter<>(MainActivity.this, android.R.layout.simple_list_item_1,arrayList);
-//                    listView.setAdapter(arrayAdapter); //пока обычный listView
-//                } catch (JSONException e) {
-//                    e.printStackTrace();
-//                }
-//
-//
-//            }
-//        });
-
-
     }
 }
