88aa22df0ab15cd918c50519ba5916b7d768cedb
TokarevIP <tokarevrab@gmail.com>
2020-04-27 16:00:18 +0300

    v3.0.2 (исправил "переслать").

--- app/src/main/java/com/example/feedler/PagedList/PostViewHolder.java
+++ app/src/main/java/com/example/feedler/PagedList/PostViewHolder.java
@@ -18,7 +18,6 @@ public class PostViewHolder extends RecyclerView.ViewHolder {
     private TextView dataText;
     private TextView groupName;
     public Button favorite;
-    private ArrayList<String> arrayList = new ArrayList<>();
 
    public PostViewHolder(View view) {
         super(view);
@@ -28,28 +27,27 @@ public class PostViewHolder extends RecyclerView.ViewHolder {
         mShare = view.findViewById(R.id.share);
         favorite = view.findViewById(R.id.favoriteButton);
 
-        mShare.setOnClickListener(v -> {
-            int position = getAdapterPosition()/6;
-            String textPost = arrayList.get(position);
-            Intent intent = new Intent(Intent.ACTION_SEND);
-            intent.setType("text/plain");
-            intent.putExtra(Intent.EXTRA_TEXT, textPost);
-            Intent chosenIntent = Intent.createChooser(intent, textPost);
-            mShare.getContext().startActivity(chosenIntent);
-        });
     }
-
 
     public void bind(Post post) {
         postText.setText(post.getPostText());
         dataText.setText(post.getDate());
         groupName.setText(post.getGroupName());
-        arrayList.add(post.getPostText());
+
         if (post.favorite){
             favorite.setBackground(favorite.getContext().getDrawable(R.drawable.grade));
         } else {
             favorite.setBackground(favorite.getContext().getDrawable(R.drawable.grade_empty));
         }
+        mShare.setOnClickListener(v -> {
+
+
+            Intent intent = new Intent(Intent.ACTION_SEND);
+            intent.setType("text/plain");
+            intent.putExtra(Intent.EXTRA_TEXT, post.getPostText());
+            Intent chosenIntent = Intent.createChooser(intent, post.getPostText());
+            mShare.getContext().startActivity(chosenIntent);
+        });
 
 
     }
