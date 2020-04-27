6b56b99c419c16ff98c2b18a23807a7deb67e234
TokarevIP <tokarevrab@gmail.com>
2020-04-27 14:38:02 +0300

    v3.0 (исправил "переслать"). Итог на рк2

--- app/src/main/java/com/example/feedler/PagedList/PostViewHolder.java
+++ app/src/main/java/com/example/feedler/PagedList/PostViewHolder.java
@@ -1,6 +1,5 @@
 package com.example.feedler.PagedList;
 
-import android.content.Context;
 import android.content.Intent;
 import android.view.View;
 import android.widget.Button;
@@ -30,8 +29,7 @@ public class PostViewHolder extends RecyclerView.ViewHolder {
         favorite = view.findViewById(R.id.favoriteButton);
 
         mShare.setOnClickListener(v -> {
-            int pos = getAdapterPosition();
-            String textPost = arrayList.get(pos);
+            String textPost = arrayList.get(0);
             Intent intent = new Intent(Intent.ACTION_SEND);
             intent.setType("text/plain");
             intent.putExtra(Intent.EXTRA_TEXT, textPost);
